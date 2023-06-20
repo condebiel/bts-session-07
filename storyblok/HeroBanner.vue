f20d7684618c51db9635b598914d23ba16004772
Biel Conde <biel.conde@terramate.io>
2023-06-20 20:29:19 +0200

    feat: initial commit

---
+++ storyblok/HeroBanner.vue
@@ -0,0 +1,40 @@
+<template>
+  <div 
+		v-editable="blok"
+		class="hero-banner"
+	>
+    <img :src="blok.imageUrl" :alt="blok.text" class="w-full h-auto">
+    <div class="overlay"></div>
+    <div class="content">
+      <p>{{ blok.text }}</p>
+    </div>
+  </div>
+</template>
+
+<script setup>
+defineProps({ blok: Object })
+</script>
+
+<style scoped>
+.hero-banner {
+  position: relative;
+}
+
+.overlay {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.5);
+}
+
+.content {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  color: #fff;
+	font-size: 2rem;
+}
+</style>

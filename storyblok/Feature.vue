24c3b26490f4391d43f2a43c8460bd88749d46cf
Biel Conde <biel.conde@terramate.io>
2023-06-20 20:45:55 +0200

    feat: update feature.vue color

--- storyblok/Feature.vue
+++ storyblok/Feature.vue
@@ -1,7 +1,7 @@
 <template>
   <div
     v-editable="blok"
-    class="w-full p-12 bg-[#f7f6fd] rounded-[5px] text-center"
+    class="w-full p-12 bg-[#ffb8b8] rounded-[5px] text-center"
   >
     <h3 class="text-2xl text-[#1d243d] font-bold">
       {{ blok.name }}
